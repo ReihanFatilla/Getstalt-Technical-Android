e895f1fb33ce543bd087b1330f1414b21c3030ed
ReihanFatilla <reihanfatilla@gmail.com>
2023-10-05 14:48:19 +0700

    [feat]: added Rent Dummy Generator

--- app/src/main/java/com/gestalt/reift/gestalt_perpustakaan/model/Rent.kt
+++ app/src/main/java/com/gestalt/reift/gestalt_perpustakaan/model/Rent.kt
@@ -8,4 +8,21 @@ data class Rent(
     val book: Book,
     val dateRent: LocalDate,
     val dateReturn: LocalDate
-)
+){
+    companion object{
+        fun generateDummy(): List<Rent> {
+            val dummyCustomer = Customer(1, "John Doe", "1234567890", "123 Main St")
+            val dummyBook = Book(1, "Sample Book", "Sample Author", 10, "Fiction")
+
+            return List(10) { index ->
+                Rent(
+                    id = index.toLong(),
+                    customer = dummyCustomer,
+                    book = dummyBook,
+                    dateRent = LocalDate.now(),
+                    dateReturn = LocalDate.now().plusDays(index.toLong())
+                )
+            }
+        }
+    }
+}
